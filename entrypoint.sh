a7f6d1033993ed94b0f9ed8b13cdebd82efe0cd8
Danny Schofield <dannyschofield28@gmail.com>
2024-05-06 14:44:05 -0400

    action: remove cdk-path as an input

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,6 @@ if [ $INPUT_APPLY = true ]; then
     apply="--appply"
 fi
 
-cdk_path=""
-if [ $INPUT_CDK_PATH ]; then
-    cdk_path="--cdk-path=$INPUT_CDK_PATH"
-fi
-
 account_tag="--account-tag=$INPUT_ACCOUNT_TAG"
 
 org_file=""
@@ -19,9 +14,9 @@ if [ $INPUT_ORGANIZATION_PATH ]; then
     org_file="--org=$INPUT_ORGANIZATION_PATH"
 fi
 
-stacks="--all-stacks"
+stacks=""
 if [ $INPUT_STACKS ]; then
     stacks="--stacks=-\"$INPUT_STACKS\""
 fi
 
-telophasecli deploy $apply $cdk_path $account_tag $org_file $stacks
+telophasecli deploy $apply $account_tag $org_file $stacks
